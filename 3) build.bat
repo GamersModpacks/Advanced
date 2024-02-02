00ba4cc2d74aa8bf8761681d0d2fb6f22138d754
Dmytro Denchenko <dima.den4enko.ok@gmail.com>
2024-02-02 18:03:19 +0100

    Beta 1.8

--- 3) build.bat
+++ 3) build.bat
@@ -1,17 +1,24 @@
 @echo off
-:: Cleaning old files.
+:: Clean old files
+echo %time% [Script] Cleaning old files...
 rmdir beta /s /q
-:: Merging two modpacks
-xcopy framework\*.* beta\ /e
-xcopy mod\*.* beta\ /e /y
-:: Removing unused files.
+:: Merge two modpacks
+echo %time% [Script] Merging two modpacks...
+xcopy framework\*.* beta\ /e /Q > nul
+xcopy mod\*.* beta\ /e /y /Q > nul
+:: Copy Changelog
+echo %time% [Script] Copying Changelog...
+copy CHANGELOG.md beta\config\fancymenu\assets\changelog.md > nul
+:: Remove unused files
+echo %time% [Script] Removing unused files...
 del beta\README.md
-del beta\CHANGELOG.md
 del beta\.gitattributes
 del beta\.packwizignore
 :: Pack Additions
+echo %time% [Script] Pack Additions...
 cd beta
-packwiz refresh
-packwiz remove xaeros-minimap
-packwiz refresh --build
+packwiz refresh > nul
+packwiz remove xaeros-minimap > nul
+packwiz refresh --build > nul
+echo %time% [Script] Done!
 @pause
