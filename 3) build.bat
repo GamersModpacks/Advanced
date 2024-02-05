9d186a3f8dffe419d20d6bafa18a501b9dd9f9e7
Dmytro Denchenko <dima.den4enko.ok@gmail.com>
2024-02-05 16:44:05 +0100

    Beta 1.9

--- 3) build.bat
+++ 3) build.bat
@@ -1,24 +1,35 @@
 @echo off
+:: Check PackFramework folder
+echo [%time%] Checking PackFramework folder...
+IF EXIST "framework" (
+    echo [%time%] Found a PackFramework folder!
+) ELSE (
+	echo [%time%] Downloading PackFramework source...
+	git clone https://github.com/Den4enko/PackFramework framework
+	cd framework
+	git checkout forge/1.19.2
+	cd ..
+)
 :: Clean old files
-echo %time% [Script] Cleaning old files...
+echo [%time%] Cleaning old files...
 rmdir beta /s /q
 :: Merge two modpacks
-echo %time% [Script] Merging two modpacks...
+echo [%time%] Merging two modpacks...
 xcopy framework\*.* beta\ /e /Q > nul
 xcopy mod\*.* beta\ /e /y /Q > nul
 :: Copy Changelog
-echo %time% [Script] Copying Changelog...
+echo [%time%] Copying Changelog...
 copy CHANGELOG.md beta\config\fancymenu\assets\changelog.md > nul
 :: Remove unused files
-echo %time% [Script] Removing unused files...
+echo [%time%] Removing unused files...
 del beta\README.md
 del beta\.gitattributes
 del beta\.packwizignore
 :: Pack Additions
-echo %time% [Script] Pack Additions...
+echo [%time%] Pack Additions...
 cd beta
 packwiz refresh > nul
 packwiz remove xaeros-minimap > nul
 packwiz refresh --build > nul
-echo %time% [Script] Done!
+echo [%time%] Done!
 @pause
